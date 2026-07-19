330ef35b4bf7e4f297944695609b96a596193e1c
Hahuynhtran <tranhh300898@sv-onuni.edu.vn>
2026-07-19 22:01:17 +0700

    Add files via upload

---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TeaShopWeb.MvcApplication" Language="C#" %>
---
+++ Global.asax.cs
@@ -0,0 +1,26 @@
+﻿using System.Web.Optimization;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;          // Thêm dòng này nếu có dùng DbInitializer
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using TeaShopWeb.Models;           // Thêm dòng này
+
+namespace TeaShopWeb
+{
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Kích hoạt tự động tạo DB + seed dữ liệu
+            Database.SetInitializer(new DbInitializer());
+        }
+    }
+}
